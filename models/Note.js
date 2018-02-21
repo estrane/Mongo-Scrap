94296a74dcf124e911f7cd73ff77c850ef99ec10
estrane <zeldrew@hotmail.com>
2018-02-20 20:35:28 -0800

    Added app and mongo DB

---
+++ models/Note.js
@@ -0,0 +1,20 @@
+// Require mongoose
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var noteSchema = new Schema({
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  noteText: String
+});
+
+var Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
